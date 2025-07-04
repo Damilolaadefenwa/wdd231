4b9fabc19d59cafe57cd3145ee94d9e2d4bf9c35
Damilolaadefenwa <ade24039@byui.edu>
2025-07-04 21:14:56 +0100

    first and second commit

---
+++ scripts/navigation.js
@@ -0,0 +1,13 @@
+// NAVIGATION RESPONSIVENESS
+    //1. Mobile View - Hamburger
+    // storing the selected elements to target my html id attribute in the button and nav.tag
+const navbutton = document.querySelector('#ham-btn');
+const navLinks = document.querySelector('#nav-bar');
+//Adding my event listener to toggle the show class off and on
+navbutton.addEventListener('click', () => {
+    navbutton.classList.toggle('show');
+    navLinks.classList.toggle('show');
+});
+
+
+  
