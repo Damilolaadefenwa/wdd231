544a5ee5986cf1174b9e1010fc52af6431ff6f98
Damilolaadefenwa <ade24039@byui.edu>
2025-07-05 19:55:18 +0100

    third commits

---
+++ scripts/dates.js
@@ -0,0 +1,10 @@
+//WDD231 FOOTER REQUIREMENT FOR DATES.
+// Get the current year for the copyright
+const copyrightYearSpan = document.getElementById("current-year");
+const currentYear = new Date().getFullYear();
+copyrightYearSpan.textContent = currentYear;
+
+// Get the last modified date of the document
+const lastModifiedSpan = document.getElementById("last-modified");
+const lastModifiedDate = document.lastModified;
+lastModifiedSpan.textContent = lastModifiedDate;
