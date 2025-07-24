042e61edc33234ea4fefb5e57eedecc666bdba4e
Damilolaadefenwa <ade24039@byui.edu>
2025-07-24 08:34:24 +0100

    third commit

---
+++ wek04/startLabTemple/js/scripts.js
@@ -0,0 +1,32 @@
+import { temples } from "../data/temples.js";
+// console.log(temples);
+
+import { url } from "../data/temples.js";
+// console.log(url);
+
+//The Query Selector References for Our HTML to display the Item
+const showHere = document.querySelector("#showHere");
+const mydialog = document.querySelector("#mydialog");
+const mytitle = document.querySelector("#mydialog h2");
+const myclose = document.querySelector("#mydialog button");
+const myinfo = document.querySelector("#mydialog p");
+// Adding the Dynamic Action
+myclose.addEventListener("click", () => mydialog.close())
+
+//----------- CREATE A FUNCTION THAT WILL LOOP THROUGH
+// THE ARRAY OF JSON ITEMS 
+function displayItem(data) {
+    console.log(data)
+    data.forEach(x => {
+        console.log(x)
+        const photo = document.createElement('img')
+        photo.src = `${url}${x.path}`
+        photo.alt = x.name
+
+        showHere.appendChild(photo)
+    })
+    
+}
+
+//START DISPLAYING ALL ITEMS by calling the function
+displayItem(temples)
